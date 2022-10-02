9dea8d7b0588a257a06988a7f037c0ffac1797fa
León Domingo <leon.domingo@gmail.com>
2022-10-02 12:44:46 +0200

    * remove ES version * update puppeteer * update fontawesome

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,7 +1,2 @@
 #!/bin/bash
-
-# ES version
-node ./pdf/to_pdf.js --url http://localdev.io/cv --pdf ./pdf/leon-domingo-cv__ES.pdf
-
-# EN version
-node ./pdf/to_pdf.js --url http://localdev.io/cv/en --pdf ./pdf/leon-domingo-cv__EN.pdf
+node ./pdf/to_pdf.js --url http://localdev.io/cv/ --pdf ./pdf/leon-domingo-cv.pdf
